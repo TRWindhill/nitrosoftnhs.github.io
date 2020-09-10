408963fab1be4f10e68435934724193bbfd5a34b
TRWindhill <yts19652@gmail.com>
2020-09-10 21:05:02 +0200

    Added new files

---
+++ NHS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NHS.Startup))]
+namespace NHS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
